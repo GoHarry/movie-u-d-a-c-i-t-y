394cbec8bfe52aa20632a53c30bb3c541ddb402d
sonu bedi <sonubedi@gmail.com>
2015-05-04 21:35:58 -0400

    movie commit 2

---
+++ README.md
@@ -0,0 +1,42 @@
+#Project-1-Movie-Trailer-Website
+Udacity Project1
+
+##DESCRIPTION
+
+-This project shows a list of movie names and information about the movies like moviename,trailer, language of the movie and poster image of the movie.
+
+-The searchbox is used to search a movie based on the moviename or the language of the movie.
+
+-We can see the trailer of the movie by clicking on it.
+
+-Bootstrap is used for the designing.
+
+###INSTALLATIONS FOR WINDOWS
+
+Python - https://www.python.org/downloads/windows/
+
+###INSTALLATIONS FOR MAC
+
+Python - https://www.python.org/downloads/mac-osx/
+
+###GENERAL NOTES
+
+1)Once the python is installed search for IDLE(GUI).(To confirm whether python is installed or not search for IDLE)
+
+2)IDLE is a program installed by python to run python code.
+
+3)To start writing your program open IDLE and then click on file select newfile(File-NewFile).
+
+4)Write Your Program and save it.(File-Save).
+
+5)Once you are done with your program Run it(Run-Run Module) or just press F5 to run your code.
+
+###FILES INCLUDED IN PROJECT
+
+1)media.py - In these file we created a class called Movie.
+
+2)entertainment.py - In these file we are creating multiple instances of the class Movie by importing the previous file media.py in which the class Movie is declared.we also need to import the file fresh_tomatoes in this file to open a html page with the list of movies.
+
+3)fresh_tomatoes.py - This file consists a function open_movie_page that takes a list of movies as input and creates an html page that shows the list of movies.These file also contains of scripts that perform the search and play youtube videos on your website and bootstrap stylesheets to apply styles to our website.
+
+so we need to run entertainment file to see the output as we have imported media and fresh_tomatoes files in entertainment file.
---
+++ media.py
@@ -0,0 +1,24 @@
+import fresh_tomatoes
+class Movie():
+	def __init__(self, title, poster_image, trailer_url):
+		self.title=title
+		self.poster_image_url=poster_image
+		self.trailer_youtube_url=trailer_url
+
+sholay = Movie("Sholay", "http://indiaopines.com/wp-content/uploads/2013/11/Sholay-poster.jpg", "https://www.youtube.com/watch?v=36SMdfwisLg")
+ddlj   = Movie("Dilwale Dulhaniya Le Jaayenge","http://www.du360.in/wp-content/uploads/2014/12/Dilwale-Dulhania-Le-Jayenge-poster.jpg",
+	"https://www.youtube.com/watch?v=RCS5NlDIqME")
+titanic = Movie("Titanic",
+	"http://www.movie-poster-artwork-finder.com/posters/titanic-poster-artwork-leonardo-dicaprio-kate-winslet-billy-zane-small.jpg",
+	"https://www.youtube.com/watch?v=Jkjrh4EWW4E")
+spiderman = Movie("The Amazing Spider-Man 2","https://pmcdeadline2.files.wordpress.com/2014/06/amazing-spider-man-2-poster__140603232341.jpg",
+	"https://www.youtube.com/watch?v=DlM2CWNTQ84")
+ironman = Movie("Iron Man 3","http://www.impawards.com/2013/posters/iron_man_three_ver11.jpg","https://www.youtube.com/watch?v=Ke1Y3P9D0Bc")
+avengers = Movie("Avengers","http://www.theaterhopper.com/wordpress/wp-content/uploads/2012/02/avengers_poster.jpg","https://www.youtube.com/watch?v=eOrNdBpGMv8")
+babysdayout = Movie("Baby's Day Out (1994)","http://ia.media-imdb.com/images/M/MV5BMjE1NTU3OTAyM15BMl5BanBnXkFtZTcwNDg0NDMyMQ@@._V1_SX640_SY720_.jpg",
+	"https://www.youtube.com/watch?v=AYkvarLxatM")
+skyfall = Movie("Skyfall","http://t2.gstatic.com/images?q=tbn:ANd9GcTSNSk0M1z_CZ1UKTnfE2nHmk4Oxqh_gKO0dAHZHwrfLX6D9Y4s",
+	"https://www.youtube.com/watch?v=A82nMyFLdtQ")
+lifeofpi = Movie("Life of Pi","http://www.impawards.com/2012/posters/life_of_pi.jpg","https://www.youtube.com/watch?v=j9Hjrs6WQ8M")
+movies = [sholay, ddlj, titanic, spiderman, ironman, avengers, babysdayout, skyfall, lifeofpi]
+fresh_tomatoes.open_movies_page(movies)
